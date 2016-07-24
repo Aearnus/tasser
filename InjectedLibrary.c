11811b0eaa0a0a2e903ab744bf4d57f8845124c2
Tyler L <tslimkemann42@gmail.com>
2016-07-24 00:25:16 -0700

    removed keypress randomization

--- InjectedLibrary.c
+++ InjectedLibrary.c
@@ -3,17 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-int getRandomKeycode() {
-    return (rand() % 120);
-}
-
-int isProgramInitialized = 0;
 void handleUniversalEvents(UniversalEvent* e) {
     if (e->event_action == KEY_DOWN) {
         printf("Key down. Keycode: %i\n", e->keycode);
-        e->keycode = getRandomKeycode();
     } else if (e->event_action == KEY_DOWN) {
         printf("Key up. Keycode: %i\n", e->keycode);
-        e->keycode = getRandomKeycode();
     }
 }
